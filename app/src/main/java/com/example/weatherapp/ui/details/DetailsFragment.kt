5b534d7d2d941654fd58d77c24eb8639d20084a7
mdt72t <patrikkarolyi@hotmail.com>
2022-01-21 19:21:31 +0100

    Add navigation

---
+++ app/src/main/java/com/example/weatherapp/ui/details/DetailsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.weatherapp.ui.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weatherapp.R
+
+class DetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DetailsFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.details_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/weatherapp/ui/list/ListFragment.kt
@@ -0,0 +1,21 @@
+package com.example.weatherapp.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weatherapp.R
+
+class ListFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
